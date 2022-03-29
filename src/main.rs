32312267c1e7388b719377c3e523a90cdea47114
MacAphon <c.a.korn@gmx.de>
2022-03-29 11:10:25 +0200

    a single sprite from a spritesheet is rendered on a screen changing color

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,41 @@
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::image::{self, LoadTexture, InitFlag};
+use sdl2::render::{Texture, WindowCanvas};
 use std::time::Duration;
-use sdl2::render::WindowCanvas;
+use sdl2::rect::{Point, Rect};
 
-fn render(canvas: &mut WindowCanvas, color: Color) {
+fn render(
+    canvas: &mut WindowCanvas,
+    color: Color,
+    texture: &Texture,
+    position: Point,
+    sprite: Rect
+) -> Result<(), String> {
     canvas.set_draw_color(color);
     canvas.clear();
+
+    let (width, height) = canvas.output_size()?;
+    let screen_position = position + Point::new(width as i32 / 2, height as i32 / 2);
+    let screen_rect = Rect::from_center(screen_position, sprite.width(), sprite.height());
+
+    canvas.copy(texture, sprite, screen_rect)?;
+
     canvas.present();
+
+    Ok(())
 }
 
 fn main() -> Result<(), String> {
+
+    // setup
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
-    let window = video_subsystem.window("HuskyRock", 1024, 512)
+    let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
+
+    let window = video_subsystem.window("HuskyRock", 800, 600)
         .position_centered()
         .build()
         .expect("could not initialize video subsystem");
@@ -22,13 +43,20 @@ fn main() -> Result<(), String> {
     let mut canvas = window.into_canvas().build()
         .expect("could not make a canvas");
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture("assets/bardo.png")?;
+
+    let position = Point::new(0, 0);
+
+    // src position in the spritesheet
+    let sprite = Rect::new(0, 0, 26, 36);
+
     let mut event_pump = sdl_context.event_pump()?;
+
     let mut i = 0;
     let mut j = 0;
 
+    // game loop
     'running: loop {
         // handle events
         for event in event_pump.poll_iter() {
@@ -50,7 +78,7 @@ fn main() -> Result<(), String> {
         i = (i + 1) % 255;
 
         // render
-        render(&mut canvas, Color::RGB(i, j, 255 - i));
+        render(&mut canvas, Color::RGB(i, j, 255 - i), &texture, position, sprite)?;
 
         // time management
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
